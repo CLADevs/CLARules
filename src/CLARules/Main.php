2531ede41966e8176efd3f928759ef2859fbdbe4
iiFlamiinBlaze <iiflamiinblaze123@gmail.com>
2022-02-23 19:17:10 -0500

    Update to PM4

--- src/CLARules/Main.php
+++ src/CLARules/Main.php
@@ -2,7 +2,7 @@
 
 /*
  * CLARules, a public rules plugin for PocketMine-MP
- * Copyright (C) 2017-2018 CLADevs
+ * Copyright (C) 2017-2022 CLADevs
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -24,62 +24,60 @@ namespace CLARules;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\item\Item;
-use pocketmine\item\WrittenBook;
-use pocketmine\Player;
+use pocketmine\item\VanillaItems;
+use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
+use function count;
+use function str_replace;
 
 class Main extends PluginBase{
-
-    private const VERSION = "v1.0.2";
-    private const PREFIX = TextFormat::GREEN . "CLARules" . TextFormat::GOLD . " > ";
-
-    public function onEnable() : void{
-        @mkdir($this->getDataFolder());
-        $this->saveDefaultConfig();
-        $this->getLogger()->info("CLARules " . self::VERSION . " by CLADevs is enabled");
-    }
-
-    public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
-        if($command->getName() === "rules"){
-            if(!$sender instanceof Player){
-                $sender->sendMessage(self::PREFIX . TextFormat::RED . "Use this command in-game");
-                return false;
-            }
-            if(!$sender->hasPermission("rules.command")){
-                $sender->sendMessage(self::PREFIX . TextFormat::RED . "You do not have permission to use this command");
-                return false;
-            }
-            if($this->getConfig()->get("rules-type") === "book"){
-                if($sender->getInventory()->canAddItem(Item::get(Item::WRITTEN_BOOK, 0, 1))){
-                    count($this->getConfig()->get("rules-pages")) > 0 ? $this->bookRules($sender) : $sender->sendMessage(TextFormat::RED . str_replace(["&", "{line}"], ["§", "\n"], $this->getConfig()->get("rules-not-found")));
-                }else{
-                    $sender->sendMessage(TextFormat::RED . str_replace(["&", "{line}"], ["§", "\n"], $this->getConfig()->get("your-inventory-full")));
-                }
-            }elseif($this->getConfig()->get("rules-type") === "message"){
-                count($this->getConfig()->get("messages")) > 0 ? $this->messageRules($sender) : $sender->sendMessage(TextFormat::RED . str_replace(["&", "{line}"], ["§", "\n"], $this->getConfig()->get("rules-not-found")));
-            }
-        }
-        return true;
-    }
-
-    private function bookRules(Player $player) : void{
-        /** @var WrittenBook $book */
-        $book = Item::get(Item::WRITTEN_BOOK, 0, 1);
-        $book->setTitle(str_replace(["&", "{line}"], ["§", "\n"], $this->getConfig()->get("rules-title")));
-        $book->setAuthor(str_replace(["&", "{line}"], ["§", "\n"], $this->getConfig()->get("rules-author")));
-
-        $rules = $this->getConfig()->get("rules-pages");
-        for($i = 0; $i < count($rules); $i++){
-            $book->setPageText($i, str_replace(["&", "{line}"], ["§", "\n"], $rules[$i]));
-        }
-        
-        $player->getInventory()->addItem($book);
-        $player->sendMessage(TextFormat::GREEN . str_replace(["&", "{line}"], ["§", "\n"], $this->getConfig()->get("book-give-success-message")));
-    }
-
-    private function messageRules(Player $player) : void{
-        foreach($this->getConfig()->get("messages") as $message) $player->sendMessage(str_replace(["&", "{line}"], ["§", "\n"], $message));
-    }
+	
+	private const PREFIX = TextFormat::GREEN . "CLARules" . TextFormat::GOLD . " > ";
+	
+	public function onEnable() : void{
+		@mkdir($this->getDataFolder());
+		$this->saveDefaultConfig();
+	}
+	
+	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
+		if($command->getName() === "rules"){
+			if(!$sender instanceof Player){
+				$sender->sendMessage(self::PREFIX . TextFormat::RED . "Use this command in-game");
+				return false;
+			}
+			if(!$sender->hasPermission("rules.command")){
+				$sender->sendMessage(self::PREFIX . TextFormat::RED . "You do not have permission to use this command");
+				return false;
+			}
+			if($this->getConfig()->get("rules-type") === "book"){
+				if($sender->getInventory()->canAddItem(VanillaItems::WRITTEN_BOOK()->setCount(1))){
+					count($this->getConfig()->get("rules-pages")) > 0 ? $this->bookRules($sender) : $sender->sendMessage(TextFormat::RED . str_replace("{line}", "\n", TextFormat::colorize($this->getConfig()->get("rules-not-found"))));
+				}else{
+					$sender->sendMessage(TextFormat::RED . str_replace("{line}", "\n", TextFormat::colorize($this->getConfig()->get("your-inventory-full"))));
+				}
+			}elseif($this->getConfig()->get("rules-type") === "message"){
+				count($this->getConfig()->get("messages")) > 0 ? $this->messageRules($sender) : $sender->sendMessage(TextFormat::RED . str_replace("{line}", "\n", TextFormat::colorize($this->getConfig()->get("rules-not-found"))));
+			}
+		}
+		return true;
+	}
+	
+	private function bookRules(Player $player) : void{
+		$book = VanillaItems::WRITTEN_BOOK()->setCount(1);
+		$book->setTitle(str_replace("{line}", "\n", TextFormat::colorize($this->getConfig()->get("rules-title"))));
+		$book->setAuthor(str_replace("{line}", "\n", TextFormat::colorize($this->getConfig()->get("rules-author"))));
+		
+		$rules = $this->getConfig()->get("rules-pages");
+		for($i = 0; $i < count($rules); $i++){
+			$book->setPageText($i, str_replace("{line}", "\n", TextFormat::colorize($rules[$i])));
+		}
+		
+		$player->getInventory()->addItem($book);
+		$player->sendMessage(TextFormat::GREEN . str_replace("{line}", "\n", TextFormat::colorize($this->getConfig()->get("book-give-success-message"))));
+	}
+	
+	private function messageRules(Player $player) : void{
+		foreach($this->getConfig()->get("messages") as $message) $player->sendMessage(str_replace("{line}", "\n", TextFormat::colorize($message)));
+	}
 }
